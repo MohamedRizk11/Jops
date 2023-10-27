4626b58eb0538b23d8f4fb4cb4c7b0e786dcb714
MohamedRizk11 <mohamedrizk1222211@gmail.com>
2023-10-27 20:39:16 +0200

    add filter & list_display

--- job/admin.py
+++ job/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from .models import Category , Job ,Company
 
 
-
-admin.site.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    search_fields=('title','loction','category','description')
+    list_filter=('vecancy','job_nature','job_nature','experience')
+    list_display=['title','loction','company','job_nature','vecancy','category']
+admin.site.register(Job,JobAdmin)
 admin.site.register(Category)
 admin.site.register(Company)
 
