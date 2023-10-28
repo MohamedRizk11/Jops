fec87bbe388484d21493a63b522831e27b2d46f3
MohamedRizk11 <mohamedrizk1222211@gmail.com>
2023-10-28 19:55:33 +0200

    add slug title

--- job/models.py
+++ job/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django_countries.fields import CountryField
 from django.utils import timezone
+from django.utils.text import slugify
 # Create your models here.
 
 
@@ -25,8 +26,12 @@ class Job(models.Model):
     job_nature=models.CharField(choices=job_type,max_length=10)
     experience=models.IntegerField()
     category=models.ForeignKey('Category',on_delete=models.SET_NULL,null=True,blank=True)
+    slug=models.SlugField(null=True,blank=True)
     def __str__(self):
         return self.title
+    def save(self,*args,**kwargs):
+        self.slug=slugify(self.title)
+        super(Job,self).save(*args,**kwargs)
 
 class Category(models.Model):
     name=models.CharField(max_length=40)
