9a208538b8a99af104b39877391c75e70edc388e
MohamedRizk11 <mohamedrizk1222211@gmail.com>
2023-10-29 17:20:25 +0200

    add class list &detail &serailizer api

--- job/api.py
+++ job/api.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import generics
 from .serailzers import Jobserailzers
 from .models import Job
 
@@ -16,3 +17,14 @@ def job_detail_api(request,id):
     job=Job.objects.get(id=id)
     data=Jobserailzers(job).data
     return Response({'Job':data})
+
+
+
+class joblistapi(generics.ListAPIView):
+    queryset=Job.objects.all()
+    serializer_class=Jobserailzers
+
+
+class jobdetailapi(generics.RetrieveAPIView):
+    queryset=Job.objects.all()
+    serializer_class=Jobserailzers    
--- job/urls.py
+++ job/urls.py
@@ -1,15 +1,15 @@
 
 from django.urls import path
 from .views import job_list,job_detail
-from .api import job_list_api ,job_detail_api
+from .api import job_list_api ,job_detail_api,joblistapi,jobdetailapi
 
 urlpatterns = [
     path('', job_list),
     path('<slug:slug>', job_detail),
 
-    path('api/list/<int:id>',job_detail_api),
+    path('api/list/<int:pk>',jobdetailapi.as_view()),
 
-    path('api/list',job_list_api)
+    path('api/list',joblistapi.as_view())
     
 
 
